be8c588d4b8a62603d5ef6bb3fdc44115a755f58
jibrail0398 <jibrailadji0398@gmail.com>
2024-12-11 15:40:08 +0700

    Membuat handler api.go

---
+++ handler/api.go
@@ -0,0 +1,95 @@
+package handler
+
+import (
+	"a21hc3NpZ25tZW50/service"
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func getTokenHuggingFace()string{
+	// Load the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// Retrieve the Hugging Face token from the environment variables
+	token := os.Getenv("myAI_Token")
+	if token == "" {
+		log.Fatal("HUGGINGFACE_TOKEN is not set in the .env file")
+	}
+	return token
+}
+
+
+func AnalyzeHandler(w http.ResponseWriter, r *http.Request) {
+
+	token := getTokenHuggingFace()
+
+	var fileService = &service.FileService{}
+	var aiService = &service.AIService{Client: &http.Client{}}
+	err := r.ParseMultipartForm(1024)
+	if err != nil {
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return
+	}
+
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Failed to read file", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Unable to read file content", http.StatusInternalServerError)
+		return
+	}
+
+	resultFile, err := fileService.ProcessFile(string(content))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	question := r.FormValue("query")
+
+	tapasRequest, errTapasRequest := aiService.AnalyzeData(resultFile, question, token)
+	if errTapasRequest != nil {
+		http.Error(w, errTapasRequest.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tapasRequest)
+}
+
+func ChatHandler(w http.ResponseWriter, r *http.Request) {
+
+	token := getTokenHuggingFace()
+	
+	var requestBody struct {
+		Query string `json:"query"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&requestBody)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	aiService := service.AIService{Client: &http.Client{}}
+
+	response, err := aiService.ChatWithAI(requestBody.Query, token)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
--- main.go
+++ main.go
@@ -1,103 +1,30 @@
 package main
 
 import (
-	"a21hc3NpZ25tZW50/service"
-	"encoding/json"
-	"io"
+	
+	"a21hc3NpZ25tZW50/handler"
 	"log"
 	"net/http"
 	"os"
 	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
 	"github.com/rs/cors"
 )
 
-// Initialize the services
-var fileService = &service.FileService{}
-var aiService = &service.AIService{Client: &http.Client{}}
+
 
 func main() {
-	// Load the .env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	// Retrieve the Hugging Face token from the environment variables
-	token := os.Getenv("myAI_Token")
-	if token == "" {
-		log.Fatal("HUGGINGFACE_TOKEN is not set in the .env file")
-	}
+	
+	
+	
 
 	// Set up the router
 	router := mux.NewRouter()
 
 	// File analyze endpoint
-	router.HandleFunc("/analyze", func(w http.ResponseWriter, r *http.Request) {
-
-		err := r.ParseMultipartForm(1024) 
-		if err != nil {
-			http.Error(w, "Unable to parse form", http.StatusBadRequest)
-			return
-		}
-
-		file, _, err := r.FormFile("file")
-		if err != nil {
-			http.Error(w, "Failed to read file", http.StatusBadRequest)
-			return
-		}
-		defer file.Close()
-
-		content, err := io.ReadAll(file)
-		if err != nil {
-			http.Error(w, "Unable to read file content", http.StatusInternalServerError)
-			return
-		}
-
-		resultFile, err := fileService.ProcessFile(string(content))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		
-		question  := r.FormValue("query")
-
-
-		tapasRequest,errTapasRequest := aiService.AnalyzeData(resultFile,question,token)
-		if errTapasRequest!=nil{
-			http.Error(w,errTapasRequest.Error(),500)
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(tapasRequest)
-	}).Methods("POST")
+	router.HandleFunc("/analyze", handler.AnalyzeHandler).Methods("POST")
 
 	// Chat endpoint
-	router.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
-		
-		var requestBody struct {
-			Query   string `json:"query"`
-		}
-
-		err := json.NewDecoder(r.Body).Decode(&requestBody)
-
-		if err != nil {
-			http.Error(w, "Invalid request body", http.StatusBadRequest)
-			return
-		}
-	
-		aiService := service.AIService{Client: &http.Client{}}
-
-		response, err := aiService.ChatWithAI( requestBody.Query, token)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(response)
-		
-	}).Methods("POST")
+	router.HandleFunc("/chat", handler.ChatHandler).Methods("POST")
 
 
 
