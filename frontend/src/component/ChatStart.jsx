bb9da827965f239dfa1a11eb1af02f40f141e573
jibrail0398 <jibrailadji0398@gmail.com>
2024-12-14 13:09:12 +0700

    Berhasil membuat halaman chatpage

--- frontend/src/component/ChatStart.jsx
+++ frontend/src/component/ChatStart.jsx
@@ -1,10 +1,9 @@
 function ChatStart(){
     return(
         <div className="chat chat-start">
-            <div className="chat-bubble">
+            <div className="chat-bubble chat-bubble-success text-white ml-4 mt-4">
                 It's over Anakin,
-                <br />
-                I have the high ground.
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae adipisci libero consectetur laborum expedita in eos asperiores, fugiat ipsum impedit dignissimos laudantium. Iure illum nesciunt atque, tenetur expedita omnis nostrum.
             </div>
         </div>
 
--- frontend/src/routes/ChatPage.jsx
+++ frontend/src/routes/ChatPage.jsx
@@ -1,7 +1,103 @@
+import {ReactComponent as ListIcon } from '../assets/list.svg';
+import { useLocation, Link } from 'react-router-dom';
+import React, { useState } from "react";
+import ChatStart from "../component/ChatStart";
+import ChatEnd from "../component/ChatEnd";
+import axios from "axios";
+import  {ReactComponent as Send} from "../assets/send.svg";
+
+
 function ChatPage(){
+
+    const [file, setFile] = useState(null);
+    const [query, setQuery] = useState("");
+    const [response, setResponse] = useState("");
+    
+    const location = useLocation();
+    const isActive = (path) => location.pathname === path;
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    const handleUpload = async () => {
+        const formData = new FormData();
+        formData.append("file", file);
+
+        try {
+        const res = await axios.post('http://localhost:8080/upload', formData, {
+            headers: {
+            'Content-Type': 'multipart/form-data',
+            },
+        });
+        console.log(res)
+        } catch (error) {
+        console.error('Error uploading file:', error);
+        }
+    };
+    
     return(
         <>
-            <div>pasa</div>
+            <div >
+                
+                <div className="drawer z-[100]">
+                    <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+                    <div className="drawer-content">
+                        
+                        <label htmlFor="my-drawer">
+                            <ListIcon className='ListIcon cursor-pointer m-2 mb-8' width={50} height={50} />
+                        </label>
+                    
+                    </div>
+                    <div className="drawer-side ">
+                        <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay z-100"></label>
+                        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                            <h1 className='text-2xl font-bold font-roboto mb-5' >History</h1>
+                            {/* Sidebar content here */}
+                            <Link
+                            to="/sidebar-item-1"
+                            className={`text-lg ${isActive('/Chat') ? 'bg-blue-500 text-white rounded-xl p-2' : ''}`}
+                            >
+                            Sidebar Item 1
+                            </Link>
+                            
+                        </ul>
+                    </div>
+                    
+                </div>
+                
+                <div className='sm:flex sm:justify-center sm:items-center sm:space-x-2'>
+                    <input 
+                        type="file" 
+                        className="mx-auto block sm:w-3/4 max-w-lg p-3 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-700 hover:bg-gray-100 transition-all duration-300 ease-in-out
+                        file:mr-4 file:py-2 file:px-4 
+                        file:rounded-full file:border-0 
+                        file:text-sm file:font-semibold 
+                        file:bg-blue-50 file:text-blue-700 
+                        hover:file:bg-blue-100 
+                        sm:m-0" 
+                        onChange={handleFileChange} 
+                    />
+                    <button 
+                        className='block mx-auto p-2 w-3/4 mt-4 sm:w-1/4 sm:mt-0 bg-sky-500 rounded-xl text-white sm:ml-0 sm:px-4' 
+                    >
+                        Upload
+                    </button>
+
+                </div>
+            </div>
+            <ChatEnd/>
+            <ChatStart/>
+
+            <div className="fixed bottom-0 left-0 right-0 w-full p-4 bg-white shadow-lg  flex items-center space-x-2 h-10">
+                <input 
+                    type="text" 
+                    className="input input-bordered  w-full mb-14 p-4" 
+                />
+                <button >
+                    <Send className='Send mb-10 ' width={40} height={40} />
+                </button>
+            </div>
         </>
     )
 }
